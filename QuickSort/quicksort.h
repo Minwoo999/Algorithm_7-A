7ad17a8e19f4048e36ed6d986727cc33f5e2a5cc
Minwoo999 <127461945+Minwoo999@users.noreply.github.com>
2023-05-07 23:15:19 +0900

    Create quicksort.h

---
+++ QuickSort/quicksort.h
@@ -0,0 +1,17 @@
+/*************************************
+ 프로그램명 : 퀵 정렬
+ 실습일 : 2023.05.07
+ 개발자 : 김민우
+**********************************/
+
+#ifndef quicksort_h
+#define quicksort_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+void q_sort(int *arr, int l, int r);
+#endif /* quicksort_h */
