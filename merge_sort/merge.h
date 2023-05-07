07a131c8f359fc7d2a72fbd56eaa746013495627
Minwoo999 <127461945+Minwoo999@users.noreply.github.com>
2023-05-07 23:03:53 +0900

    Create merge.h

---
+++ merge_sort/merge.h
@@ -0,0 +1,21 @@
+/*************************************
+ 프로그램명 : 합병 정렬
+ 실습일 : 2023.05.07
+ 개발자 : 김민우
+**********************************/
+
+#ifndef merge_h
+#define merge_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge_sort(int *arr, int l, int r);
+
+
+//TC
+void TC1_Tennumber(void);
+void TC2_Twentyonenumber(void);
+void TC3_hundrednumber(void);
+
+#endif /* merge_h */
