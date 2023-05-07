1bcf6f348158e2b13aa8929348470bc0d3b2c332
Minwoo999 <127461945+Minwoo999@users.noreply.github.com>
2023-05-07 22:58:28 +0900

    Create selection.h

---
+++ QucikSort/selection.h
@@ -0,0 +1,19 @@
+/*************************************
+ 프로그램명 : 퀵 정렬
+ 실습일 : 2023.05.07
+ 개발자 : 김민우
+**********************************/
+
+#ifndef selection_h
+#define selection_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int selection(int *arr, int l, int r, int k, int n);
+int *filelead(char *name, int *n);
+void filesave(char *name, int a);
+
+#endif /* selection_h */
